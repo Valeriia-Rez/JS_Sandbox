fe5228dab33de59466b5639b94176023603a763b
Valeriia-Rez <Valeria09122016@gmail.com>
2019-11-29 14:20:22 +0200

    add singleton pattern

--- app.js
+++ app.js
@@ -28,24 +28,59 @@
 //console.log(UICtrl.text);
 
 //REVEALING MODULE PATTERN
-const ItemCtrl = (function(){
-    let data = [];
+//const ItemCtrl = (function(){
+    //let data = [];
 
-    function add(item){
-        data.push(item);
-        console.log("Item Added...");
+    //function add(item){
+        //data.push(item);
+        //console.log("Item Added...");
+    //}
+    //function get(id){
+        //return data.find(item => {
+            //return item.id === id;
+        //});
+    //}
+    //return{
+        //add: add,
+        //get: get
+    //}
+//})();
+
+//ItemCtrl.add({id:1, name: "John"});
+//ItemCtrl.add({id:2, name: "Mike"});
+//console.log(ItemCtrl.get(1));
+
+const Singleton = (function() {
+    let instance;
+
+    function createInstance() {
+        const object = new Object({name: "Brad"});
+        return object;
     }
-    function get(id){
-        return data.find(item => {
-            return item.id === id;
-        });
-    }
+
     return{
-        add: add,
-        get: get
+        getInstance:function() {
+            if(!instance){
+                instance = createInstance();
+            }
+            return instance;
+        }
     }
 })();
+ const instanceA = Singleton.getInstance();
+ console.log(instanceA);
 
-ItemCtrl.add({id:1, name: "John"});
-ItemCtrl.add({id:2, name: "Mike"});
-console.log(ItemCtrl.get(1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
