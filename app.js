7599bc71188cd1e3e0b9f88aadf0a3e612123af8
Valeriia-Rez <Valeria09122016@gmail.com>
2019-11-30 21:14:55 +0200

    change app.js,index.html,add appes6.js and add Singleton and Observer Pattern

--- app.js
+++ app.js
@@ -50,25 +50,121 @@
 //ItemCtrl.add({id:2, name: "Mike"});
 //console.log(ItemCtrl.get(1));
 
-const Singleton = (function() {
-    let instance;
+//const Singleton = (function() {
+    //let instance;
 
-    function createInstance() {
-        const object = new Object({name: "Brad"});
-        return object;
-    }
+    //function createInstance() {
+        //const object = new Object({name: "Brad"});
+        //return object;
+    //}
 
-    return{
-        getInstance:function() {
-            if(!instance){
-                instance = createInstance();
+    //return{
+        //getInstance:function() {
+            //if(!instance){
+                //instance = createInstance();
+            //}
+            //return instance;
+        //}
+    //}
+//})();
+ //const instanceA = Singleton.getInstance();
+ //console.log(instanceA);
+
+function EventObserver(){
+    this.observers = [];
+}
+
+EventObserver.prototype = {
+    subscribe: function(fn) {
+        this.observers.push(fn);
+        console.log(`You are now subscribed to ${fn.name}`);
+    },
+    unsubscribe: function(fn) {
+        this.observers = this.observers.filter(function(item){
+            if(item !== fn){
+                return item;
             }
-            return instance;
-        }
+        });
+    console.log(`You are now unsubscribed from ${fn.name}`);
+    },
+
+    fire: function() {
+        this.observers.forEach(function(item) {
+            item.call();
+        });
     }
-})();
- const instanceA = Singleton.getInstance();
- console.log(instanceA);
+}
+
+const click = new EventObserver();
+
+//Event listeners
+document.querySelector(".sub-ms").addEventListener("click", function() {
+    click.subscribe(getCurMilliseconds);
+});
+
+document.querySelector(".unsub-ms").addEventListener("click", function() {
+    click.unsubscribe(getCurMilliseconds);
+});
+
+document.querySelector(".sub-s").addEventListener("click", function() {
+    click.subscribe(getCurSeconds);
+});
+
+document.querySelector(".unsub-s").addEventListener("click", function() {
+    click.unsubscribe(getCurSeconds);
+});
+
+document.querySelector(".fire").addEventListener("click", function() {
+    click.fire();
+});
+
+//Click Handler
+const getCurMilliseconds = function() {
+    console.log(`Current Milliseconds:${new Date().getMilliseconds()}`);
+}
+
+const getCurSeconds = function() {
+    console.log(`Current Seconds:${new Date().getSeconds()}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
